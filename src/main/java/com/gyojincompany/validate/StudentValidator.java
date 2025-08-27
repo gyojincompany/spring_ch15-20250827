872b3cd6e242b1cdcb4bc1c8378a18838c0bd59a
gyojincompany <luckyred@gmail.com>
2025-08-27 14:54:35 +0900

    Validator 인터페이스로 validation 하기 기능 추가

---
+++ src/main/java/com/gyojincompany/validate/StudentValidator.java
@@ -0,0 +1,38 @@
+package com.gyojincompany.validate;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+public class StudentValidator implements Validator {
+
+	@Override
+	public boolean supports(Class<?> clazz) { //검증할 객체(studetnDto)의 클래스 타입 정보
+		// TODO Auto-generated method stub
+		return StudentDto.class.isAssignableFrom(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		// TODO Auto-generated method stub
+		StudentDto studentDto = (StudentDto) target;
+		
+		String id = studentDto.getId(); //유저가 가입할 때 입력한 아이디
+		String pw = studentDto.getPw(); //유저가 가입할 때 입력한 비밀번호
+		int age = studentDto.getAge(); //유저가 가입할 때 입력한 나이
+		
+		if(id.strip().isEmpty() || id == null) { //두 조건 중에 한개라도 참이면 error
+			System.out.println("에러가 발생된 아이디 : " + id);
+			errors.rejectValue("id", "trouble001"); //(에러가 발생한 필드 이름, 에러코드(메시지))
+		}
+		
+		if(pw.strip().isEmpty() || pw == null) { //두 조건 중에 한개라도 참이면 error
+			System.out.println("에러가 발생된 비밀번호 : " + id);
+			errors.rejectValue("pw", "trouble002"); //(에러가 발생한 필드 이름, 에러코드(메시지))
+		}
+		
+		if(age > 19 || age <0) { //나이가 19세 초과일 경우 학생회원 자격 없음->error, 나이값이 음수일 경우 error
+			errors.rejectValue("age", "trouble003"); //(에러가 발생한 필드 이름, 에러코드(메시지))
+		}
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/join.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>    
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>학생 회원 가입</title>
+</head>
+<body>
+	<h2>학생 회원 가입</h2>
+	<hr>
+	<form action="joinOk">
+		아이디 : <input type="text" name="id"><br><br>
+		비밀번호 : <input type="password" name="pw"><br><br>
+		이름 : <input type="text" name="name"><br><br>
+		나이 : <input type="text" name="age"><br><br>
+		<input type="submit" value="학생회원가입">
+	</form>
+	<hr>
+	<c:if test="${not empty error}">
+		<span style="color:red;">회원가입불가! 다시 확인하시고 가입하세요.</span>
+	</c:if>
+</body>
+</html>
